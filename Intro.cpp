526765e1af077be5f732bf40f848c1b251b04dc2
Marmik <marmikpatel246@gmail.com>
2025-12-30 19:18:37 +0530

    Functions and Start of STL Lib

--- Intro.cpp
+++ Intro.cpp
@@ -1,14 +1,36 @@
 #include<iostream> 
+#include<bits/stdc++.h>
 using namespace std;
 
+
+// // Function --> Pass by value and pass by reference
+// void doSomething (string s) {
+//         s[0] = 'T';
+//         cout << s << endl; // Output will be "Tarmik"
+// }
+
+// void doSomethingRef (string &s) {
+//         s[0] = 'T';
+//         cout << s << endl; // Output will be "Tarmik"
+// }
 int main() {
     // cout << "Hello, World!" << endl;
-    string s = "Marmik";
-    cout << s << endl;
-    int len = s.length();
-    cout << len<< endl;
-    s[len - 1] = 's';
-    cout<< s << endl;
-    return 0;
+    // string s = "Marmik";
+    // cout << s << endl;
+    // int len = s.length();
+    // cout << len<< endl;
+    // s[len - 1] = 's';
+    // cout<< s << endl;
+  
 
+
+   
+
+    
+    // string name = "Marmik";
+    // doSomething(name);
+    // cout << name << endl; // Output will be "Marmik" because string is passed by value
+    // doSomethingRef(name);   
+    // cout << name << endl; // Output will be "Tarmik" because string is passed by reference
+    return 0;
 }
