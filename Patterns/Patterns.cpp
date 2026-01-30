e69f8e05231106fcc701b1ab0f029ee3ffb00dfd
Marmik <marmikpatel246@gmail.com>
2026-01-30 20:19:44 +0530

    update code

--- Patterns/Patterns.cpp
+++ Patterns/Patterns.cpp
@@ -187,6 +187,183 @@ void print10(int n){
     
 }
 
+void print11(int n){
+
+    int start = 1;
+    for (int i = 0; i < n; i++)
+    {
+        if( i %2 == 0) start = 1;
+        else start = 0;
+        for (int j = 0; j <= i ; j++)
+        {
+            cout << start<< " ";
+            start = 1 - start;
+        }
+        cout << endl;
+        
+    }
+    
+}
+
+void print12(int n){
+    int space = 2 *(n-1);
+    for (int i = 1; i <= n; i++)
+    {
+        //num
+        for(int j = 1; j<=i; j++){
+            cout << j;
+        }
+
+        //space
+        for (int j = 1; j <= space ; j++)
+        {
+            cout << " ";
+        }
+        
+
+        //num
+        for(int j = i; j>=1; j--){
+            cout << j;
+        }
+        cout << endl;
+        space -= 2;
+    }
+    
+}
+
+void print13(int n){
+
+    int num = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << num <<" ";
+            num += 1;
+        }
+        cout << endl;
+
+    }
+    
+}
+
+void print14(int n){
+    for (int i = 0; i < n; i++)
+    {
+        for (char ch = 'A'; ch <= 'A' + i; ch++)
+        {
+            cout << ch <<" ";
+        }
+        cout << endl;
+    }
+    
+}
+
+void print15(int n){
+    for (int i = 0; i < n; i++)
+    {
+        for (char j = 'A'; j <= 'A' + (n - i -1); j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+
+void print16(int n){
+    for (int i = 0; i < n; i++)
+    {
+        char ch = 'A' + i;
+        for (int j = 0; j <= i; j++)
+        {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+    
+}
+
+void print17(int n){
+    for (int i = 0; i < n; i++)
+    {
+        // space
+        for (int j = 0; j < n-i-1; j++)
+        {
+            cout << " ";
+        }
+        
+        // characters
+        char ch = 'A';
+        int breakpoint = (2*i+1)/2;
+        for (int j = 1; j <= 2*i+1; j++)
+        {
+            cout << ch;
+            if (j <= breakpoint) ch++;
+            else ch--;
+            
+        }
+
+        // space
+        for (int j = 0; j < n-i-1; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+void print19(int n){
+    int spaces = 0;
+    for (int i = 0; i < n; i++)
+    {
+        //stars
+        for (int j = 1; j <= n-i ; j++)
+        {
+            cout << "*";
+        }
+        
+        //spaces
+        for (int j = 0; j < spaces ; j++)
+        {
+           cout << " ";
+        }
+        
+        //stars
+        for (int j = 1; j <= n-i ; j++)
+        {
+            cout << "*";
+        }
+        spaces += 2;
+        cout << endl;
+
+    }
+
+    spaces = 2*n - 2;
+    for (int i = 1; i <= n; i++)
+    {
+        //stars
+        for (int j = 1; j <= i ; j++)
+        {
+            cout << "*";
+        }
+        
+        //spaces
+        for (int j = 0; j < spaces ; j++)
+        {
+           cout << " ";
+        }
+        
+        //stars
+        for (int j = 1; j <= i ; j++)
+        {
+            cout << "*";
+        }
+        spaces -= 2;
+        cout << endl;
+
+    }
+}
+
 
 int main(){
     int n;
@@ -200,7 +377,13 @@ int main(){
     // print7(n);
     // print8(n);
     // print9(n);
-    print10(n);
+    // print10(n);
+    // print11(n);
+    // print12(n);
+    // print13(n);
+    // print13(n);
+    // print14(n);
+    print19(n);
 
     return 0;
 }
