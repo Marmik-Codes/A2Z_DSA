6da556f66281d697d552148188781f10d7391405
Marmik <marmikpatel246@gmail.com>
2026-02-09 22:22:39 +0530

    allest added
    Signed-off-by: Marmik <marmikpatel246@gmail.com>

--- Arrays/secondLargest.cpp
+++ Arrays/secondLargest.cpp
@@ -20,6 +20,25 @@ int secondLargest(vector<int>& nums){
     return slargest;
 }
 
+int secondsmallest(vector<int>& nums){
+    int smallest = nums[0];
+    int ssmallest = INT16_MAX;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (nums[i] < smallest)
+        {
+            ssmallest = smallest;
+            smallest = nums[i];
+        }
+        else if(nums[i] != smallest && nums[i] < ssmallest){
+            ssmallest = nums[i];
+        }
+        
+    }
+    return ssmallest;
+}
+
 int main(){
     cout << "Finding the second largest element in an array." << endl;
     int n;
@@ -28,6 +47,15 @@ int main(){
     for(int i = 0; i < n; i++){
         cin >> arr[i];
     }
+    cout << "The array is : ";
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
     int slargest = secondLargest(arr);
     cout << "The second largest element in the array is : " << slargest << endl;
+    
+    int ssmallest = secondsmallest(arr);
+    cout << "The second smallest element in the array is : " << ssmallest << endl;
+
+    return 0;
 }
